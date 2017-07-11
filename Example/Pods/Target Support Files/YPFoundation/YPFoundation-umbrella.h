a13e7242475db32a948b3575a9840a183dddf344
辛亚鹏 <xinyapeng@baijiahulian.com>
2017-07-11 18:05:56 +0800

    0.2.0

--- Example/Pods/Target Support Files/YPFoundation/YPFoundation-umbrella.h
+++ Example/Pods/Target Support Files/YPFoundation/YPFoundation-umbrella.h
@@ -10,6 +10,31 @@
 #endif
 #endif
 
+#import "BJCFAction.h"
+#import "BJCommonDefines.h"
+#import "BJHL-Foundation-iOS.h"
+#import "BJCFError.h"
+#import "NSString+BJCFError.h"
+#import "BJCFCacheManagerTool.h"
+#import "BJCFFileManagerTool.h"
+#import "BJCFJockey.h"
+#import "BJCFKeychain.h"
+#import "BJCFKeychainQuery.h"
+#import "BJCFJSONUtils.h"
+#import "BJCFTimer.h"
+#import "NSArray+BJCF.h"
+#import "NSDate+BJCF.h"
+#import "NSDateFormatter+BJCF.h"
+#import "NSDictionary+BJCF.h"
+#import "NSDictionary+BJCFDataValue.h"
+#import "NSObject+BJCFBlockKVO.h"
+#import "NSString+BJCF.h"
+#import "NSString+BJCFEncoding.h"
+#import "NSString+BJCFMD5.h"
+#import "NSString+BJCFSize.h"
+#import "NSString+BJCFVersion.h"
+#import "NSURL+BJCFURLQuery.h"
+#import "UIDevice+BJCFIdentifier.h"
 
 FOUNDATION_EXPORT double YPFoundationVersionNumber;
 FOUNDATION_EXPORT const unsigned char YPFoundationVersionString[];
